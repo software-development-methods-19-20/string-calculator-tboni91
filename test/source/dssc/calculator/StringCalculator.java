291946c1af607f7103fc627717f60c0148a59bbc
taz00 <tbonifazi91@gmail.com>
2019-11-13 11:04:32 +0100

    EmptyString, oneNumber, twoNumbers

---
+++ test/source/dssc/calculator/StringCalculator.java
@@ -0,0 +1,15 @@
+package dssc.calculator;
+
+public class StringCalculator {
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else if (numbers.contains(",")) {
+            String[] tokens = numbers.split(",");
+            return Integer.valueOf(tokens[0]) + Integer.valueOf(tokens[1]);
+        } else {
+            return Integer.valueOf(numbers);
+        }
+    }
+
+}
--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -1,4 +1,28 @@
 package test;
 
+import dssc.calculator.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class AddNumbersTest {
+    @Test
+    void emptyString() {
+        assertEquals(0, StringCalculator.add(""));
+        assertThat(StringCalculator.add(""), is(0));
+    }
+
+    @Test
+    void oneNumber() {
+        assertThat(StringCalculator.add("1"), is(1));
+    }
+
+    @Test
+    void twoNumbers() {
+        assertThat(StringCalculator.add("1,2"), is(3));
+    }
+
 }
